147b2766aaf53e2b8ddc90963b012e192cda7c71
narendrasinghrathore <rathoreofficialemail@gmail.com>
2020-10-18 20:40:22 +0530

    Basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <div class="logo">
+      <div>NSR</div>
+    </div>
+    <Home msg="Welcome to Your Vue.js App" />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  margin: 0;
+  font-weight: 1em;
+}
+main {
+  background-color: #dedede;
+  display: flex;
+  height: 100vh;
+  padding: 10px;
+  flex-direction: column;
+}
+.logo {
+  border-radius: 50%;
+  background-color: black;
+  color: white;
+  height: 100px;
+  width: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3vh;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="heading">
+    <h1>Narendra Singh Rathore</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.heading {
+  display: flex;
+  height: 200px;
+  align-items: center;
+  justify-content: center;
+  font-weight: 1em;
+}
+h1 {
+  font-size: 3em;
+}
+</style>
